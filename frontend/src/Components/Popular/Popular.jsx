0845a09ac7566754429b5cff1caa31cf9cc108a0
amanraj464667 <154000730+amanraj464667@users.noreply.github.com>
2025-03-01 16:11:44 +0530

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './Popular.css'
 import Item from '../Item/Item'
 
-const url='http://localhost:4000';
+const url='https://ecomm-backend-uylx.onrender.com';
 
 const Popular = () => {
    
